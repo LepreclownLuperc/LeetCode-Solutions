c5fa618a47db03ae0f64962b30433a1eab3156a4
Dmytro Perchykov <mgumskull4@gmail.com>
2026-05-16 15:03:05 +0300

    Time: 0 ms (100%), Space: 46.6 MB (78.53%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cs
@@ -0,0 +1,14 @@
+using System.Linq;
+
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        int k = 0;
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if(nums[i] != val)
+                nums[k++] = nums[i];
+        }
+
+        return k;
+    }
+}
